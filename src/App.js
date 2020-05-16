0789b2eaaa7f83fc1f972d1421c2303037ff055e
ajboet <ajboet@hotmail.com>
2020-05-16 09:49:36 -0400

    Begin global notifications

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./Components/NavBar/Navbar.js"
-
+import Alert from 'react-bootstrap/Alert'
 import Body from './Components/Body/Body'
 
 function App() {
-
+  const [show,setShow] = React.useState(true)
   const logged = localStorage.getItem('token')
   const [token, setToken] = React.useState(logged !== null ? true : false);
 
@@ -16,6 +16,11 @@ function App() {
 
   return (
     <div className="App">
+      <Alert variant="info"
+        onClose={() => setShow(false)}
+        dismissible show={show} style={{marginBottom:0,zIndex:1060}}>
+        Woohoo, you're reading this text in a Alert!
+      </Alert>
       <Navbar token={token} changeToken={changeToken}></Navbar>
       <Body />
     </div>
