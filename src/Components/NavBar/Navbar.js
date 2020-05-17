5887a9e7f549f15bd70c6df1bbd0279b9142d855
ajboet <ajboet@hotmail.com>
2020-05-17 18:12:55 -0400

    Delete var guest in navbar

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -11,13 +11,12 @@ const NavBase = (props) => {
 
   const logout = () => {
     axiosInstance.get('logout')
-      .then(response => {
-        props.showMessage({
-          mode: 'success',
-          text: response.data.message
-        })
+    .then(response => {
+      props.showMessage({
+        mode:'success',
+        text:response.data.message
       })
-
+    })
     delete axiosInstance.defaults.headers['Authorization']
     localStorage.removeItem('token')
     localStorage.removeItem('me')
@@ -25,37 +24,36 @@ const NavBase = (props) => {
   }
 
   return (
-    <Navbar variant="dark" className="justify-content-between" style={{ zIndex: 2, position: 'sticky', top: 0 }}>
+    <Navbar variant="dark" className="justify-content-between" style={{zIndex:2,position:'sticky',top:0}}>
       {/* <Navbar.Brand href="#">Yummy <b> Pizza </b> </Navbar.Brand> */}
       <div></div>
       <div>
-        <Nav style={{ display: 'inline-block' }}>
-          <Nav.Item style={{ display: 'inline-block' }} onClick={() => props.setCurrency(props.currency === 'USD' ? 'EUR' : 'USD')}>
+        <Nav style={{display:'inline-block'}}>
+          <Nav.Item style={{display:'inline-block'}} onClick={() => props.setCurrency(props.currency === 'USD' ? 'EUR':'USD')}>
             <Nav.Link eventKey="currency">
-              <i className={`${props.currency === 'USD' ?
-                'fa fa-dollar-sign' :
-                'fa fa-euro-sign'}`
-              }
-                style={{ fontSize: 19, color: 'white' }}></i>
+              <i className={`${props.currency === 'USD'?
+                    'fa fa-dollar-sign':
+                    'fa fa-euro-sign'}`
+                }
+              style={{fontSize:19,color:'white'}}></i>
             </Nav.Link>
           </Nav.Item>
         </Nav>
-        <NavDropdown title="Menu" id="nav-dropdown" style={{ display: 'inline-block' }}>
+        <NavDropdown title="Menu" id="nav-dropdown" style={{display:'inline-block'}}>
           {
-            props.guest ? [
+            !props.token ? [
               <Login {...props} key="login"></Login>,
               <User {...props} key="register" mode="Register"></User>
-            ] :
-              [
-                <User {...props} key="profile" mode="My profile"></User>,
-                // <NavDropdown.Item key="history" eventKey="history">History</NavDropdown.Item>,
-                <Historical {...props} key="historical" eventKey="history" />,
-                <NavDropdown.Item
-                  onClick={logout}
-                  key="logout" eventKey="logout">
-                  Logout
+            ]:
+            [
+              <User {...props} key="profile" mode="My profile"></User>,
+              <Historical {...props} key="history" eventKey="history"></Historical>,
+              <NavDropdown.Item 
+                onClick={logout}
+                key="logout" eventKey="logout">
+                Logout
               </NavDropdown.Item>
-              ]
+            ]
           }
         </NavDropdown>
       </div>
