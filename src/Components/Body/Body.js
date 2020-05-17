44a96a7a82e8495d9e7feac0e18267e4ada6da85
ajboet <ajboet@hotmail.com>
2020-05-17 01:25:36 -0400

    Added section Delivery Info. Editing svg to backgraun of menu and a part of delivery J

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './Hero/Hero'
 import Menu from './Menu/Menu'
+import Delivery from './Delivery/Delivery'
 
 const Body = (props) => {
   return (
     <div>
       <Hero />
       <Menu />
+      <Delivery />
     </div>
   )
 }
